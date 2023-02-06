afc3cfae47e2e43faa344e391d0b72014f24651e
daniel-san17 <daniel.rotstein17@gmail.com>
2023-02-06 14:32:42 -0500

    working in users.js file

--- routes/users.js
+++ routes/users.js
@@ -6,3 +6,13 @@ import {
 } from "../controllers/users.js";
 import { verifyToken } from "../middleware/auth.js";
 
+const router = express.Router();
+
+/* READ (Get) */
+router.get("/:id", verifyToken, getUser);
+router.get("/:id/friends", verifyToken, getUserFriends);
+
+/* UPDATE (Patch) */
+router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
+
+export default router;
