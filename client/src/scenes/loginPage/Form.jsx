ce55ff8d96a481fb03800f62d34ece689791ab5c
daniel-san17 <daniel.rotstein17@gmail.com>
2023-02-08 14:58:37 -0500

    almost done with loginPage - Form.jsx

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -47,5 +47,22 @@ const initialValuesLogin = {
 };
 
 export const Form = () => {
+    const [pageType, setPageType] = useState("login");
+    const { palette } = useTheme();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const isNonMobile = useMediaQuery("(min-width: 600px)");
+    const isLogin = pageType === "login";
+    const isRegister = pageType === "register";
 
-}
+    const handleFormSubmit = async (values, onSubmitProps) => {};
+
+        return (
+            <Formik
+                onSubmit={handleFormSubmit}
+                initialValues={isLogin ? initialValuesLogin : initialValuesRegister}
+                validationSchema={isLogin ? loginSchema : registerSchema}
+            >
+            </Formik>
+        )
+};
