9e29a808f9ed57f907dfb8d822649df4d2aaf972
daniel-san17 <daniel.rotstein17@gmail.com>
2023-02-08 14:34:20 -0500

    imported some files for loginPage - Form.jsx

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -7,7 +7,7 @@ import {
     Typography,
     useTheme,
 } from "@mui/material";
-import { EditOutlinedIcon } from "@mui/icons-material";
+import { EditOutlinedIcon } from "@mui/icons-material/EditOutlined";
 import { Formik } from "formik";
 import * as yup from "yup";
 import { useNavigate } from "react-router-dom";
