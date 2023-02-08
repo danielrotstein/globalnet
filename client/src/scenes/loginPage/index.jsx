562407ab0eb6e8eb4bfa0dfd8006243187b5f016
daniel-san17 <daniel.rotstein17@gmail.com>
2023-02-08 14:20:10 -0500

    working on loginPage index.jsx

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,8 +1,32 @@
 import React from "react";
+import { 
+    Box,
+    Typography,
+    useTheme,
+    useMediaQuery,
+} from "@mui/material";
 
 const LoginPage = () => {
+    const theme = useTheme();
+    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+    
     return (
-        <div></div>
+        <Box>
+            <Box 
+                width="100%" 
+                backgroundColor={theme.palette.background.alt} 
+                p="1rem 6%" 
+                textAlign="center"
+            >
+                <Typography
+                    fontWeight="bold"
+                    fontSize="32px"
+                    color="primary"
+                >
+                    GlobalNet
+                </Typography>
+            </Box>
+        </Box>
     )
 };
 
