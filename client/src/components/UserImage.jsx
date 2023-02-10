df8e8eba50a0115cd6c58073e7cc4135bd0fee83
daniel-san17 <daniel.rotstein17@gmail.com>
2023-02-10 12:04:09 -0500

    created new file: UserImage.jsx in components directory

---
+++ client/src/components/UserImage.jsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+
+const UserImage = ({ image, size="60px"}) => {
+    return (
+        <Box>
+            
+        </Box>
+    )
+};
+
+export default UserImage
