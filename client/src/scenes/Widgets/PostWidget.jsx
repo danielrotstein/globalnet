d2dd24a277a5abe2fa6ca710226dca159cca1891
daniel-san17 <daniel.rotstein17@gmail.com>
2023-02-10 16:31:09 -0500

    importing in PostWidget.jsx and also created neew file: Friend.jsx in Components

--- client/src/scenes/Widgets/PostWidget.jsx
+++ client/src/scenes/Widgets/PostWidget.jsx
@@ -1,9 +1,27 @@
-const { Box } = require("@mui/system")
+import React from "react";
+import {
+    ChatBubbleOutlineOutlined,
+    FavoriteBorderOutlined,
+    FavoriteOutlined,
+    ShareOutlined,
+} from "@mui/icons-material";
+import { 
+    Box, 
+    Divider, 
+    IconButton, 
+    Typography, 
+    useTheme 
+} from "@mui/material";
+import FlexBetween from "../../components/FlexBetween";
+import Friend from "../../components/Friend";
+import WidgetWrapper from "../../components/WidgetWrapper";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPost } from "state";
 
 const PostWidget = () => {
     return (
         <Box>
-
         </Box>
     )
 }
