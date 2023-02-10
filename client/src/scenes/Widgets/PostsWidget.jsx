a0851b2c1d82c5c0dc1e9a497ac602992c0e90ae
daniel-san17 <daniel.rotstein17@gmail.com>
2023-02-10 16:17:54 -0500

    added All Posts feed API call function in PostsWidget.jsx file

--- client/src/scenes/Widgets/PostsWidget.jsx
+++ client/src/scenes/Widgets/PostsWidget.jsx
@@ -9,7 +9,15 @@ const PostsWidget = ({ userId, isProfile = false }) => {
     const posts = useSelector((state) => state.posts);
     const token = useSelector((state) => state.token);
 
-
+    const getPosts = async () => {
+        const response = await fetch("http://localhost:3001/posts", 
+        {
+            method: "GET",
+            headers: { Authorization: `Bearer ${token}` },
+        });
+        const data = await response.json();
+        dispatch(setPosts({ posts: data }));
+    }
 };
 
 export default PostsWidget;
