22b0b245c8f361f5becd027681ae723549ddcd4b
daniel-san17 <daniel.rotstein17@gmail.com>
2023-02-10 13:41:32 -0500

    working in HomePage index.jsx

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,11 +1,28 @@
-import { Box } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
 import React from "react";
+import { useSelector } from "react-redux";
+import UserWidget from "scenes/Widgets/UserWidgets.jsx";
 import Navbar from "../Navbar/index.jsx";
 
 const HomePage = () => {
+    const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+    const { _id, picturePath } = useSelector((state) => state.user);
+
     return (
         <Box>
             <Navbar />
+            <Box
+                width="100%"
+                padding="2rem 6%"
+                display={isNonMobileScreens ? "flex" : "block"}
+                gap="0.5rem"
+                justifyContent="space-between"
+            >
+                <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+                    <UserWidget />
+                </Box>
+
+            </Box>
         </Box>
     )
 };
