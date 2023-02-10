c8a20032247369683630094c4bbc1e20236ffb99
daniel-san17 <daniel.rotstein17@gmail.com>
2023-02-10 16:13:29 -0500

    working on PostsWidget.jsx file

--- client/src/scenes/Widgets/PostsWidget.jsx
+++ client/src/scenes/Widgets/PostsWidget.jsx
@@ -4,3 +4,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { setPosts } from "state";
 import PostWidget from "./PostWidget";
 
+const PostsWidget = ({ userId, isProfile = false }) => {
+    const dispatch = useDispatch();
+    const posts = useSelector((state) => state.posts);
+    const token = useSelector((state) => state.token);
+
+
+};
+
+export default PostsWidget;
