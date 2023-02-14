881ce0b5708e7face38c0790aed40818f4973de5
daniel-san17 <daniel.rotstein17@gmail.com>
2023-02-14 13:07:49 -0500

    adding typography to styling part in Friend.jsx

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -52,7 +52,25 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
                         navigate(0);
                     }}
                 >
-                    
+                    <Typography
+                        color={main}
+                        variant="h5"
+                        fontWeight="500"
+                        sx={{
+                            "&:hover": {
+                                color: palette.primary.light,
+                                cursor: "pointer"
+                            }
+                        }}
+                    >
+                        {name}
+                    </Typography>
+                    <Typography
+                        color={medium}
+                        fontSize="0.75rem"
+                    >
+                        {subtitle}
+                    </Typography>
                 </Box>
             </FlexBetween>
         </FlexBetween>
