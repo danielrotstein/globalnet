0bb5ff21a76b39054ec62b00137ce8b334a91563
daniel-san17 <daniel.rotstein17@gmail.com>
2023-02-15 17:11:30 -0500

    typo in imported paths in FriendListWidget.jsx file

--- client/src/scenes/Widgets/FriendListWidget.jsx
+++ client/src/scenes/Widgets/FriendListWidget.jsx
@@ -1,16 +1,17 @@
+import React from "react";
 import { Box, 
     Typography, 
     useTheme 
 } from "@mui/material";
-import Friend from "components/Friend";
-import WidgetWrapper from "components/WidgetWrapper";
+import Friend from "../../components/Friend";
+import WidgetWrapper from "../../components/WidgetWrapper";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setFriends } from "state";
+import { setFriends } from "../../state";
 
 const FriendListWidget = ({ userId }) => {
     const dispatch = useDispatch();
-    const palette = useTheme();
+    const { palette } = useTheme();
     const token = useSelector((state) => state.token);
     const friends = useSelector((state) => state.user.friends);
 
