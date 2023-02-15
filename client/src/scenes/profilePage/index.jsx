720781c25125c62043a20a4bb4979e6281278445
daniel-san17 <daniel.rotstein17@gmail.com>
2023-02-15 17:34:10 -0500

    working in ProfilePage index.jsx file

--- client/src/scenes/profilePage/index.jsx
+++ client/src/scenes/profilePage/index.jsx
@@ -10,8 +10,32 @@ import PostsWidget from "../../scenes/Widgets/PostsWidget";
 import UserWidget from "../../scenes/Widgets/UserWidget";
 
 const ProfilePage = () => {
+    const [user, setUser] = useState(null);
+    const { userId } = useParams();
+    const token = useSelector((state) => state.token);
+    const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+
+    const getUser = async () => {
+        const response = await fetch(
+            `http://localhost:3001/users/${userId}`, 
+            {
+                method: "GET",
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }
+        );
+        const data = await response.json();
+        setUser(data);
+    };
+
+    useEffect(() => {
+        getUser();
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+
     return (
-        <div></div>
+        <div>ProfilePage</div>
     )
 };
 
