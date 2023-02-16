eaba698490e3288dcc9918713035a093c38300ae
daniel-san17 <daniel.rotstein17@gmail.com>
2023-02-16 16:03:49 -0500

    modified HomePage import path in App.js file again

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import React from 'react'
 import HomePage from "scenes/HomePage";
-import LoginPage from "./scenes/LoginPage";
-import ProfilePage from "./scenes/ProfilePage";
+import LoginPage from "scenes/LoginPage";
+import ProfilePage from "scenes/ProfilePage";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
-import { themeSettings} from "./theme";
+import { themeSettings } from "theme";
 
 function App() {
   const mode = useSelector((state) => state.mode);
