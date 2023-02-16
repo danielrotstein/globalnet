caf63ef81bfd7a94dadd9e7beb2c757c9cf4cee6
daniel-san17 <daniel.rotstein17@gmail.com>
2023-02-16 16:49:26 -0500

    changed ProfilePage name to profilePage

--- client/src/scenes/profilePage/index.jsx
+++ client/src/scenes/profilePage/index.jsx
@@ -3,11 +3,11 @@ import { Box, useMediaQuery } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import Navbar from "../../scenes/Navbar";
-import FriendListWidget from "../../scenes/Widgets/FriendListWidget";
+import Navbar from "../Navbar";
+import FriendListWidget from "../Widgets/FriendListWidget";
 // import MyPostWidget from "../../scenes/Widgets/MyPostWidget";
-import PostsWidget from "../../scenes/Widgets/PostsWidget";
-import UserWidget from "../../scenes/Widgets/UserWidget";
+import PostsWidget from "../Widgets/PostsWidget";
+import UserWidget from "../Widgets/UserWidget";
 
 const ProfilePage = () => {
     const [user, setUser] = useState(null);
