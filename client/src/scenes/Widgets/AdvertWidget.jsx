e825ecc20d0f983c6557b3c449b700a8f0a269d3
daniel-san17 <daniel.rotstein17@gmail.com>
2023-02-22 17:05:52 -0500

    testing to deploy part8

--- client/src/scenes/Widgets/AdvertWidget.jsx
+++ client/src/scenes/Widgets/AdvertWidget.jsx
@@ -3,56 +3,36 @@ import { Typography, useTheme } from "@mui/material";
 import FlexBetween from "../../components/FlexBetween";
 import WidgetWrapper from "../../components/WidgetWrapper";
 
-
 const AdvertWidget = () => {
-    const { palette } = useTheme();
-    const dark = palette.neutral.dark;
-    const main = palette.neutral.main;
-    const medium = palette.neutral.medium;
+  const { palette } = useTheme();
+  const dark = palette.neutral.dark;
+  const main = palette.neutral.main;
+  const medium = palette.neutral.medium;
 
-    return (
-        <WidgetWrapper>
-            <FlexBetween>
-                <Typography
-                    color={dark}
-                    variant="h5"
-                    fontWeight="500"
-                >
-                    Sponsored
-                </Typography>
-                <Typography
-                    color={medium}
-                >
-                    Create Ad
-                </Typography>
-            </FlexBetween>
-            <img 
-                width="100%"
-                height="auto"
-                alt="advert"
-                src="http://localhost:3001/assets/coding.jpeg"
-                style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
-            />
-            <FlexBetween>
-                <Typography 
-                    color={main}
-                >
-                    CodeZilla Bootcamp
-                </Typography>
-                <Typography 
-                    color={medium}
-                >
-                    codezilla.com
-                </Typography>
-            </FlexBetween>
-            <Typography
-                color={medium}
-                m="0.5rem 0"
-            >
-                Your pathway to your new career in software engineering starts here!
-            </Typography>
-        </WidgetWrapper>
-    );
+  return (
+    <WidgetWrapper>
+      <FlexBetween>
+        <Typography color={dark} variant="h5" fontWeight="500">
+          Sponsored
+        </Typography>
+        <Typography color={medium}>Create Ad</Typography>
+      </FlexBetween>
+      <img
+        width="100%"
+        height="auto"
+        alt="advert"
+        src={process.env.PUBLIC_URL + "/assets/coding.jpeg"}
+        style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
+      />
+      <FlexBetween>
+        <Typography color={main}>CodeZilla Bootcamp</Typography>
+        <Typography color={medium}>codezilla.com</Typography>
+      </FlexBetween>
+      <Typography color={medium} m="0.5rem 0">
+        Your pathway to your new career in software engineering starts here!
+      </Typography>
+    </WidgetWrapper>
+  );
 };
 
 export default AdvertWidget;
